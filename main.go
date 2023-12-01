30091a7c48336fcd2b10019ad0c0f6badc6c6c1d
Dэnnis <109353595+willdiealone@users.noreply.github.com>
2023-12-01 19:56:14 +0400

    initial create

---
+++ main.go
@@ -0,0 +1,135 @@
+package main
+
+import (
+	"fmt"
+	"github.com/fatih/color"
+	"math/rand"
+	"time"
+)
+
+const NumbersOfPizzas = 10
+
+var pizzasMade, pizzasFailed, total int
+
+type Producer struct {
+	data chan PizzaOrder
+	quit chan chan error
+}
+
+type PizzaOrder struct {
+	pizzaNumber int
+	message     string
+	success     bool
+}
+
+func (p *Producer) Close() error {
+	ch := make(chan error)
+	p.quit <- ch
+	return <-ch
+}
+
+func pizzeria(pizzaMaker *Producer) {
+	i := 0
+	for {
+		currentPizza := makePizza(i)
+		if currentPizza != nil {
+			i = currentPizza.pizzaNumber
+			select {
+			case pizzaMaker.data <- *currentPizza:
+			case quitChan := <-pizzaMaker.quit:
+				close(pizzaMaker.data)
+				close(quitChan)
+				return
+			}
+		}
+	}
+}
+
+func makePizza(pizzaNumber int) *PizzaOrder {
+	pizzaNumber++
+	if pizzaNumber <= NumbersOfPizzas {
+		delay := rand.Intn(5) + 1
+		fmt.Printf("Received an order #%d!\n", pizzaNumber)
+		rnd := rand.Intn(12) + 1
+		msg := ""
+		success := false
+
+		if rnd < 5 {
+			pizzasFailed++
+		} else {
+			pizzasMade++
+		}
+		total++
+
+		fmt.Printf("Making pizza #%d. It will take %d seconds...\n", pizzaNumber, delay)
+		time.Sleep(time.Duration(delay) * time.Second)
+
+		if rnd <= 2 {
+			msg = fmt.Sprintf("*** We ran out of ingredients for pizza #%d!", pizzaNumber)
+		} else if rnd <= 4 {
+			msg = fmt.Sprintf("*** The cook quit while making for pizza #%d!", pizzaNumber)
+		} else {
+			success = true
+			msg = fmt.Sprintf("*** Pizza order #%d is ready!", pizzaNumber)
+		}
+
+		pizzaOrder := PizzaOrder{
+			pizzaNumber: pizzaNumber,
+			message:     msg,
+			success:     success,
+		}
+		return &pizzaOrder
+	}
+	return &PizzaOrder{
+		pizzaNumber: pizzaNumber,
+	}
+}
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+
+	color.Cyan("The Pizzeria is open for business!")
+	color.Cyan("----------------------------------")
+
+	pizzaJob := &Producer{
+		data: make(chan PizzaOrder),
+		quit: make(chan chan error),
+	}
+
+	go pizzeria(pizzaJob)
+
+	for i := range pizzaJob.data {
+		if i.pizzaNumber <= NumbersOfPizzas {
+			if i.success {
+				color.Green(i.message)
+				color.Green("Order #%d is out for delivery!", i.pizzaNumber)
+			} else {
+				color.Red(i.message)
+				color.Red("The customer is really mad!")
+			}
+		} else {
+			color.Cyan("Done making pizzas...")
+			err := pizzaJob.Close()
+			if err != nil {
+				color.Red("*** Error closing channel ***", err)
+			}
+		}
+	}
+	color.Cyan("-------------------")
+	color.Cyan("Done for the day!!!")
+
+	color.Cyan("We made %d pizzas, but failed to make %d, with %d attempts in total", pizzasMade, pizzasFailed, total)
+
+	switch {
+	case pizzasFailed > 9:
+		color.Red("It was an awful day...")
+	case pizzasFailed > 6:
+		color.Yellow("It was not a very bad day...")
+	case pizzasFailed > 4:
+		color.Yellow("It was okay day.")
+	case pizzasFailed > 2:
+		color.Green("It was a pretty good day.")
+	default:
+		color.Green("It was a great day!!!")
+	}
+}
